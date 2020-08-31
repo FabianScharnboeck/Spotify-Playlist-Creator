6bab8a31f22e89da948340f2bd141688df95a0c1
scharnboec <fabian.scharnboeck@t-online.de>
2020-08-31 15:54:44 +0200

    Changed: "get_liked_youtube_videos()"

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import json
+
+import spotipy
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from google_auth_oauthlib import flow
+from spotipy.oauth2 import SpotifyClientCredentials
+import youtube_dl
+
+with open("client_secret_spotify.json") as json_file:
+    spotify_credentials = json.load(json_file)
+
+SPOTIFY_CLIENT_ID = spotify_credentials["client_id_spotify"]
+SPOTIFY_CLIENT_SECRET = spotify_credentials["client_secret_spotify"]
+auth_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
 
 # Returns the liked Youtube Videos
 def get_liked_youtube_videos():
@@ -14,17 +27,26 @@ def get_liked_youtube_videos():
 
     request = service.videos().list(
         part="snippet,contentDetails,statistics",
-        myRating="like",
+        myRating="like"
     )
 
     response = request.execute()
+    for video in response["items"]:
+        url = "https://www.youtube.com/watch?v={}".format(video["id"])
+        video = youtube_dl.YoutubeDL({}).extract_info(url, download=False)
+        song_name = video["track"]
+        artist = video["artist"]
+        song_info = {
+            "url": url,
+            "song_name": song_name,
+            "artist": artist,
+            "spotify_uri": get_spotify_uri(song_name, artist)
+        }
+    return song_info
 
-    titles: list = []
-    for video in response['items']:
-        titles.append(video['snippet']['title'])
 
-    return titles
-
+def get_spotify_uri(song_name, artist):
+    pass
 
 
 likedVideos = get_liked_youtube_videos()
